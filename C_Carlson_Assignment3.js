d78291908912231bc1e8125f63050ac0768be250
Chris Carlson <christopher.s.carlson@wmich.edu>
2019-02-21 13:44:45 -0500

    Initiated NPM and gitignore

--- C_Carlson_Assignment3.js
+++ C_Carlson_Assignment3.js
@@ -3,6 +3,16 @@ let fs = require('fs');
 
 
 
+let processFile = function(filename, dataHandlerFunction){
+	fs.readFile(filename, cb(err, data){
+		if(err){
+			console.error(`Error Processing File ${filename}.\nError: ${err}`);
+		}else{
+			let dataArray = data.toString();
+		}
+	});
+}
+
 let getFileName = function(){
 	return process.argv[2];
 }
